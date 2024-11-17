7c361b0ae8140bd022d0dd4554773185a0c3aebd
Luciano Amato <luciano2697@outlook.com.ar>
2024-11-17 01:17:09 -0300

    tsconfig.json config verification

--- types/express.d.ts
+++ types/express.d.ts
@@ -1,12 +1,24 @@
-import { Users } from "@prisma/client";
+// express.d.ts
+import { Request } from 'express';
 
 declare global {
   namespace Express {
     interface Request {
-      user?: number;
+      user?: number; // Or define a more specific type for user
     }
   }
 }
+
+
+// import { Users } from "@prisma/client";
+
+// declare global {
+//   namespace Express {
+//     interface Request {
+//       user?: number;
+//     }
+//   }
+// }
 
 // import * as express from 'express';
 
